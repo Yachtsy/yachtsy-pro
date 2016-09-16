59865a464f43460c09f7c975d9cf241e1feff8cf
Borys Duda <borys.duda0928@gmail.com>
2016-09-17 00:50:58 +0800

    make requests in order of most recent first.

--- app/pages/requests/requests.ts
+++ app/pages/requests/requests.ts
@@ -36,7 +36,7 @@ export class RequestsPage {
 
       if (snapshot.exists()) {
         var requestData = snapshot.val();
-
+        var curTime = new Date().getTime();
         //console.log(requestData);
         var newRequests = [];
         Object.keys(requestData).map((key) => {
@@ -44,10 +44,22 @@ export class RequestsPage {
           var currentRequest = requestData[key];
           if (!(currentRequest.quote || currentRequest.hired || currentRequest.cancelled)) {
             currentRequest['id'] = key;
+            currentRequest['pasttime'] = this.getPastTimeString(curTime - currentRequest.date) + ' ago';
             newRequests.push(currentRequest);
           }
         });
         this.ngZone.run(() => {
+          for (var i = 0; i < newRequests.length; i++) {
+            for (var j = i + 1; j < newRequests.length; j++) {
+              if (newRequests[i].date < newRequests[j].date) {
+                var tmp = {};
+                Object.assign(tmp, newRequests[i]);
+                newRequests[i] = newRequests[j];
+                newRequests[j] = tmp;
+              }
+            }
+          }
+
           this.requests = newRequests;
         });
 
@@ -56,6 +68,32 @@ export class RequestsPage {
 
     }
     );
+  }
+
+  getPastTimeString(duration) {
+    var dur = (duration - duration % 1000) / 1000;
+    var ss, mm, hh, dd, oo, yy;
+
+    ss = dur % 60; dur = (dur - ss) / 60;
+    mm = dur % 60; dur = (dur - mm) / 60;
+    hh = dur % 24; dur = (dur - hh) / 24;
+    dd = dur % 30; dur = (dur - dd) / 30;
+    oo = dur % 12; yy = (dur - oo) / 12;
+
+    if (yy > 0)
+      return yy + 'y';
+    else if (oo > 0)
+      return oo + 'm';
+    else if (dd > 0)
+      return dd + 'd';
+    else if (hh > 0)
+      return hh + 'h';
+    else if (mm > 0)
+      return mm + 'm';
+    else if (ss > 0)
+      return ss + 's';
+    else
+      return 'now';
   }
 
 
