53e5cf541a0780f58d107807fe9362d9b833ee11
alexmady <alexmady@gmail.com>
2016-09-26 22:25:32 +0100

    show keyboard accessory bar

--- app/app.ts
+++ app/app.ts
@@ -275,7 +275,7 @@ export class MyApp {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
-      Keyboard.hideKeyboardAccessoryBar(true);
+      //Keyboard.hideKeyboardAccessoryBar(true);
       Keyboard.disableScroll(true);
 
       GlobalService.windowHeight = window.innerHeight;
